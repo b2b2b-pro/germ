7f72619028a72dc78517fc52cae948f6d7653f2d
maris-cyber <maris.sabirov@gmail.com>
2023-03-05 19:11:46 +0700

    Инициализация проекта

---
+++ src/main/java/pro/b2b2b/germ/GermApplication.java
@@ -0,0 +1,13 @@
+package pro.b2b2b.germ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GermApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/b2b2b/germ/GermApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.b2b2b.germ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
